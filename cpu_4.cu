fa2dcaefde863550bab6f1d1a068db7a704b4806
Jian Sun <sunjxan@gmail.com>
2024-12-05 18:02:28 +0800

    Update cpu_4.cu

--- cpu_4.cu
+++ cpu_4.cu
@@ -4,54 +4,74 @@
 
 void reduce(const real *A, size_t size, real *result)
 {
-    real sum = 0.0;
+    if (!size) {
+        *result = 0.0;
+        return;
+    }
+
     size_t l = 0, r = size;
+    // top - 1 是栈顶元素指针
     size_t ls[30], rs[30], top = 0, last_l = 0;
     real vs[30];
     while (true) {
         size_t mid = (l + r) >> 1;
         if (top && ls[top - 1] == l && rs[top - 1] == r) {
+            // lr匹配，不是第一次到达该结点
             if (l == last_l) {
+                // 是从左分支回溯，下一步进入右分支
                 l = mid;
             } else {
+                // 是从右分支回溯，下一步回溯
+                real v = vs[top - 1];
                 --top;
+                // 如果父结点是根结点，退出
                 if (!top) {
-                    sum = vs[0];
                     break;
                 }
+                // 准备回溯后的上下文
                 last_l = l;
                 l = ls[top - 1];
                 r = rs[top - 1];
                 if (l == last_l) {
-                    vs[top - 1] = vs[top];
+                    // 以左分支身份回溯
+                    vs[top - 1] = v;
                 } else {
-                    vs[top - 1] += vs[top];
+                    // 以右分支身份回溯
+                    vs[top - 1] += v;
                 }
             }
-            continue;
-        }
-        if (l == mid) {
+        } else if (l + 1 == r) {
+            // 第一次到达该结点
+            real v = A[l];
+            // 结点只包含一个元素，没有入栈不用递减top
             if (!top) {
-                sum = A[l];
+                // 如果该结点是根结点，退出
+                vs[0] = v;
                 break;
             }
+            // 下一步回溯，准备回溯后的上下文
             last_l = l;
             l = ls[top - 1];
             r = rs[top - 1];
             if (l == last_l) {
-                vs[top - 1] = A[l];
+                // 以左分支身份回溯
+                vs[top - 1] = v;
             } else {
-                vs[top - 1] += A[l];
+                // 以右分支身份回溯
+                vs[top - 1] += v;
             }
-            continue;
+        } else {
+            // 第一次到达该结点
+            // 分左右分支
+            ls[top] = l;
+            rs[top] = r;
+            ++top;
+            // 不回溯不用设置last_l
+            // 下一步进入左分支
+            r = mid;
         }
-        ls[top] = l;
-        rs[top] = r;
-        vs[top] = 0;
-        ++top;
-        r = mid;
     }
-    *result = sum;
+    *result = vs[0];
 }
 
 int main()
