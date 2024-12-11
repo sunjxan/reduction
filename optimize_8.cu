1acec28843aecb4ec2debc25cdf44711b17f1b4c
sunjian <sunjxan@gmail.com>
2024-12-11 16:47:56 +0800

    cuda_9

--- optimize_8.cu
+++ optimize_8.cu
@@ -4,15 +4,26 @@
 
 using namespace cooperative_groups;
 
-// 使用模板核函数，指定block大小作为模板参数
+// 一个核函数中，两次在Warp内使用协作组做顺序配对的折半归约
 
-template<unsigned blockSize>
+__device__ __forceinline__ real warpReduce(real v, thread_block_tile<32> g) {
+    v += g.shfl_down(v, 16);
+    v += g.shfl_down(v, 8);
+    v += g.shfl_down(v, 4);
+    v += g.shfl_down(v, 2);
+    v += g.shfl_down(v, 1);
+    return v;
+}
+
 __global__ void kernel(const real *A, size_t size, real *B)
 {
     unsigned tid = threadIdx.x, bid = blockIdx.x, bdx = blockDim.x, idx = bid * bdx + tid;
+    unsigned laneIdx = tid % warpSize, warpIdx = tid / warpSize, warp_count = DIVUP(bdx, warpSize);
     extern __shared__ real s_a[];
     if (idx >= size) {
-        s_a[tid] = 0.0;
+        if (!laneIdx) {
+            s_a[warpIdx] = 0.0;
+        }
         return;
     }
 
@@ -22,44 +33,18 @@ __global__ void kernel(const real *A, size_t size, real *B)
         pos += thread_count;
         v += A[pos];
     }
-    s_a[tid] = v;
+
+    thread_block_tile<32> g = tiled_partition<32>(this_thread_block());
+    v = warpReduce(v, g);
+
+    if (!laneIdx) {
+        s_a[warpIdx] = v;
+    }
     __syncthreads();
 
-    if (blockSize >= 1024) {
-        if (tid < 512) {
-            s_a[tid] += s_a[tid + 512];
-        }
-        __syncthreads();
-    }
-    if (blockSize >= 512) {
-        if (tid < 256) {
-            s_a[tid] += s_a[tid + 256];
-        }
-        __syncthreads();
-    }
-    if (blockSize >= 256) {
-        if (tid < 128) {
-            s_a[tid] += s_a[tid + 128];
-        }
-        __syncthreads();
-    }
-    if (blockSize >= 128) {
-        if (tid < 64) {
-            s_a[tid] += s_a[tid + 64];
-        }
-        __syncthreads();
-    }
-
-    if (tid < 32) {
-        s_a[tid] += s_a[tid + 32];
-
-        v = s_a[tid];
-        thread_block_tile<32> g = tiled_partition<32>(this_thread_block());
-        v += g.shfl_down(v, 16);
-        v += g.shfl_down(v, 8);
-        v += g.shfl_down(v, 4);
-        v += g.shfl_down(v, 2);
-        v += g.shfl_down(v, 1);
+    if (tid < warpSize) {
+        v = tid < warp_count ? s_a[laneIdx] : 0.0;
+        v = warpReduce(v, g);
     }
 
     if (!tid) {
@@ -69,51 +54,19 @@ __global__ void kernel(const real *A, size_t size, real *B)
 
 void reduce(const real *d_A, size_t size, real *h_result)
 {
-    // 以1:times的比例估算需要的线程数groups，block_size应是2的整数幂，且大于等于64
+    // 以1:times的比例估算需要的线程数groups，block_size应是2的整数幂，且大于等于32
     unsigned times = 10, groups = DIVUP(size, times), block_size = 1024, grid_size = DIVUP(groups, block_size);
     size_t B_size = grid_size * real_size;
 
     real *d_B = nullptr;
     CHECK(cudaMalloc(&d_B, B_size));
 
-    switch(block_size) {
-        case 1024:
-            kernel<1024><<<grid_size, block_size, block_size * real_size>>>(d_A, size, d_B);
-            break;
-        case 512:
-            kernel<512><<<grid_size, block_size, block_size * real_size>>>(d_A, size, d_B);
-            break;
-        case 256:
-            kernel<256><<<grid_size, block_size, block_size * real_size>>>(d_A, size, d_B);
-            break;
-        case 128:
-            kernel<128><<<grid_size, block_size, block_size * real_size>>>(d_A, size, d_B);
-            break;
-        case 64:
-            kernel<64><<<grid_size, block_size, block_size * real_size>>>(d_A, size, d_B);
-            break;
-    }
+    kernel<<<grid_size, block_size, DIVUP(block_size, 32) * real_size>>>(d_A, size, d_B);
     CHECK(cudaGetLastError());
     CHECK(cudaDeviceSynchronize());
 
     unsigned block_size2 = 512;
-    switch(block_size2) {
-        case 1024:
-            kernel<1024><<<1, block_size2, block_size2 * real_size>>>(d_B, grid_size, d_B);
-            break;
-        case 512:
-            kernel<512><<<1, block_size2, block_size2 * real_size>>>(d_B, grid_size, d_B);
-            break;
-        case 256:
-            kernel<256><<<1, block_size2, block_size2 * real_size>>>(d_B, grid_size, d_B);
-            break;
-        case 128:
-            kernel<128><<<1, block_size2, block_size2 * real_size>>>(d_B, grid_size, d_B);
-            break;
-        case 64:
-            kernel<64><<<1, block_size2, block_size2 * real_size>>>(d_B, grid_size, d_B);
-            break;
-    }
+    kernel<<<1, block_size2, DIVUP(block_size2, 32) * real_size>>>(d_B, grid_size, d_B);
     CHECK(cudaGetLastError());
     CHECK(cudaDeviceSynchronize());
 
--- optimize_9.cu
+++ optimize_9.cu
@@ -4,12 +4,34 @@
 
 using namespace cooperative_groups;
 
-// 一个核函数中，两次在Warp内使用协作组做顺序配对的折半归约
+// 使用模板核函数，指定block大小作为模板参数
 
+template<unsigned blockSize>
+__device__ __forceinline__ real warpReduce(real v, thread_block_tile<32> g) {
+    if (blockSize >= 32) {
+        v += g.shfl_down(v, 16);
+    }
+    if (blockSize >= 16) {
+        v += g.shfl_down(v, 8);
+    }
+    if (blockSize >= 8) {
+        v += g.shfl_down(v, 4);
+    }
+    if (blockSize >= 4) {
+        v += g.shfl_down(v, 2);
+    }
+    if (blockSize >= 2) {
+        v += g.shfl_down(v, 1);
+    }
+    return v;
+}
+
+template<unsigned blockSize>
 __global__ void kernel(const real *A, size_t size, real *B)
 {
     unsigned tid = threadIdx.x, bid = blockIdx.x, bdx = blockDim.x, idx = bid * bdx + tid;
     unsigned laneIdx = tid % warpSize, warpIdx = tid / warpSize;
+    const unsigned warp_count = DIVUP(blockSize, 32);
     extern __shared__ real s_a[];
     if (idx >= size) {
         if (!laneIdx) {
@@ -26,24 +48,15 @@ __global__ void kernel(const real *A, size_t size, real *B)
     }
 
     thread_block_tile<32> g = tiled_partition<32>(this_thread_block());
-    v += g.shfl_down(v, 16);
-    v += g.shfl_down(v, 8);
-    v += g.shfl_down(v, 4);
-    v += g.shfl_down(v, 2);
-    v += g.shfl_down(v, 1);
+    v = warpReduce<blockSize>(v, g);
 
     if (!laneIdx) {
         s_a[warpIdx] = v;
     }
     __syncthreads();
 
-    if (tid < 32) {
-        v = tid < bdx/32 ? s_a[laneIdx] : 0.0;
-        v += g.shfl_down(v, 16);
-        v += g.shfl_down(v, 8);
-        v += g.shfl_down(v, 4);
-        v += g.shfl_down(v, 2);
-        v += g.shfl_down(v, 1);
+    if (tid < warp_count) {
+        v = warpReduce<warp_count>(s_a[laneIdx], g);
     }
 
     if (!tid) {
@@ -60,12 +73,50 @@ void reduce(const real *d_A, size_t size, real *h_result)
     real *d_B = nullptr;
     CHECK(cudaMalloc(&d_B, B_size));
 
-    kernel<<<grid_size, block_size, block_size / 32 * real_size>>>(d_A, size, d_B);
+    switch(block_size) {
+        case 1024:
+            kernel<1024><<<grid_size, block_size, DIVUP(block_size, 32) * real_size>>>(d_A, size, d_B);
+            break;
+        case 512:
+            kernel<512><<<grid_size, block_size, DIVUP(block_size, 32) * real_size>>>(d_A, size, d_B);
+            break;
+        case 256:
+            kernel<256><<<grid_size, block_size, DIVUP(block_size, 32) * real_size>>>(d_A, size, d_B);
+            break;
+        case 128:
+            kernel<128><<<grid_size, block_size, DIVUP(block_size, 32) * real_size>>>(d_A, size, d_B);
+            break;
+        case 64:
+            kernel<64><<<grid_size, block_size, DIVUP(block_size, 32) * real_size>>>(d_A, size, d_B);
+            break;
+        case 32:
+            kernel<32><<<grid_size, block_size, DIVUP(block_size, 32) * real_size>>>(d_A, size, d_B);
+            break;
+    }
     CHECK(cudaGetLastError());
     CHECK(cudaDeviceSynchronize());
 
     unsigned block_size2 = 512;
-    kernel<<<1, block_size2, block_size2 / 32 * real_size>>>(d_B, grid_size, d_B);
+    switch(block_size2) {
+        case 1024:
+            kernel<1024><<<1, block_size2, DIVUP(block_size2, 32) * real_size>>>(d_B, grid_size, d_B);
+            break;
+        case 512:
+            kernel<512><<<1, block_size2, DIVUP(block_size2, 32) * real_size>>>(d_B, grid_size, d_B);
+            break;
+        case 256:
+            kernel<256><<<1, block_size2, DIVUP(block_size2, 32) * real_size>>>(d_B, grid_size, d_B);
+            break;
+        case 128:
+            kernel<128><<<1, block_size2, DIVUP(block_size2, 32) * real_size>>>(d_B, grid_size, d_B);
+            break;
+        case 64:
+            kernel<64><<<1, block_size2, DIVUP(block_size2, 32) * real_size>>>(d_B, grid_size, d_B);
+            break;
+        case 32:
+            kernel<32><<<1, block_size2, DIVUP(block_size2, 32) * real_size>>>(d_B, grid_size, d_B);
+            break;
+    }
     CHECK(cudaGetLastError());
     CHECK(cudaDeviceSynchronize());
 
